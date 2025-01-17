91b097ad3a922e2f2700e2e008bfb043b647c408
salemovic <jakihop889@gamil.com>
2025-01-17 15:31:34 +0300

    Add project files.

---
+++ LeetCodeProblemSolving/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCodeProblemSolving;
+
+//Console.WriteLine(Solution.IsSubsequence("abc", "ahbgdc"));
+//Console.WriteLine(Solution.IsSubsequence("axc", "ahbgdc"));
+//Console.WriteLine(Solution.IsSubsequence("ab", "baab"));
+
+//Console.WriteLine(Solution.IsPalindrome("A man, a plan, a canal: Panama"));
+//Console.WriteLine(Solution.IsPalindrome("race a car"));
+
+
+Console.WriteLine(Solution.LengthOfLastWord("Hello World"));//5
+Console.WriteLine(Solution.LengthOfLastWord("   fly me   to   the moon  "));//4
+Console.WriteLine(Solution.LengthOfLastWord("luffy is still joyboy"));//6
---
+++ LeetCodeProblemSolving/Solution.cs
@@ -0,0 +1,80 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCodeProblemSolving
+{
+    public static class Solution
+    {
+
+
+
+        public static int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            int length = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    length++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return length;
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            if (s == " " || string.IsNullOrEmpty(s)) return true;
+
+
+            string pattern = @"[^a-zA-Z0-9]";
+            var text = Regex.Replace(s, pattern, replacement: "").ToLower();
+            var reversedText = string.Empty;
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversedText += text[i];
+            }
+
+            return text == reversedText;
+
+
+        }
+
+        public static bool IsSubsequence(string s, string t)
+        {
+
+            if (s.Length == 0) return true;
+
+            if (s.Length > t.Length) return false;
+
+            int i = 0, j = 0;
+
+            while (j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                    j++;
+                    if (i == s.Length)
+                        return true;
+                }
+                else
+                {
+                    j++;
+
+                }
+
+            }
+
+            return false;
+
+
+        }
+    }
+}
